b8a30b5ef98affa2d4cf4e166a51de00484de35d
Raiyan <raiyan.sharif.1234@gmail.com>
2021-04-14 06:48:30 +0600

    refactor

--- src/BasicTestCases.java
+++ src/BasicTestCases.java
@@ -9,6 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
@@ -39,32 +40,81 @@ public class BasicTestCases {
 		driver.findElement(By.xpath("//div[@class='rate-table-filter']/form/div[1]/div[1]/ul/li/div[4]")).click();
 		Assert.assertEquals(driver.findElement(By.xpath("//input[@data-ng-model='currencyExchangeVM.filter.from_amount']")).getText(), "");
 		List<WebElement> options = driver.findElements(By.xpath("//div[@class='ng-scope']//tbody/tr"));
+//		List<DataXY> dataXY =  DataXY;
+		List<String> xData = new ArrayList<String>();
+		List<String> yData = new ArrayList<String>();
 		for(WebElement option : options) {
-//			System.out.println();
-			String data = option.getText();
-			String listOfData[] = data.split(" ");
-			double y = Double.parseDouble(listOfData[5].replaceAll(",", ""));
-//			double y = Double.parseDouble(listOfData[4].replaceAll(",", ""));
+			List<WebElement> listDataX = option.findElements(By.xpath("//td[5]"));
+			List<WebElement> listDataY = option.findElements(By.xpath("//td[4]"));
+			DataXY d = new DataXY();
 			
-			System.out.println(y);
-//			double actual = 
-			String d[] = listOfData[6].replaceAll("[,]", "").split("\\(");
-			String actual =d[1].replaceAll("\\)", "");
-			if(!d[0].replaceAll("\\)", "").contains("-")) {
-				double x = Double.parseDouble(d[0].replaceAll("\\)", ""));
-				System.out.println(x);
-				System.out.println(actual);
-				String expected = df.format(x-y);
+			for(WebElement data:listDataX) {
+				d.x = data.getText();
+//				System.out.println("X: "+d.x);
+				xData.add(d.x);
 				
-				System.out.println(expected);
-				Assert.assertEquals(actual, expected,"Does Not Match");
+			}
+			for(WebElement data:listDataY) {
+				d.y = data.getText();
+//				System.out.println("Y: "+d.y);
+				yData.add(d.y);
+				
 			}
 			
+//			if(d.s == true) {
+//				System.out.println(d.x);
+//				System.out.println(d.y);
+//			}
+			
+			
 		}
+		int c=0;
+		for(String i:xData) {
+			String x,y,r;
+			xData.get(c);
+			yData.get(c);
+			if(!xData.get(c).equals("-") && (!yData.get(c).equals("-"))) {
+				String d[] = xData.get(c).replaceAll("[,]", "").split("\\(");
+				x = d[0];
+				r = d[1].replaceAll("\\)", "");
+				y = yData.get(c).replaceAll("[,]", "");
+				String expected = df.format(Double.parseDouble(x)-Double.parseDouble(y));
+//				System.out.println("Actual: "+r);
+//				System.out.println("Expected: "+expected);
+				Assert.assertEquals(r, expected,"Not Matched");
+			}
+			c++;
+		}
+//		for(WebElement option : options) {
+////			System.out.println();
+//			String data = option.getText();
+//			String listOfData[] = data.split(" ");
+//			System.out.print(data);
+//			System.out.println(listOfData[6].equals("-") );
+//			if((!listOfData[6].equals("-"))) {
+//				String d[] = listOfData[6].replaceAll("[,]", "").split("\\(");
+//				String actual =d[1].replaceAll("\\)", "");
+//				double x = Double.parseDouble(d[0].replaceAll("\\)", ""));
+//				double y = Double.parseDouble(listOfData[5].replaceAll(",", ""));
+////				System.out.println(x);
+////				System.out.println(y);
+//				System.out.println(actual);
+//				String expected = df.format(x-y);
+//				
+//				System.out.println(expected);
+//				Assert.assertEquals(actual, expected,"Does Not Match");
+//			}
+//			
+//		}
 		
 		
 	}
 	
 	
 
+}
+class DataXY{
+	public String x;
+	public String y;
+	public boolean s;
 }
